b77d9ca6cb5b3ca645096d6d69fb508aec765d47
Eric Dai <ericdai2000@gmail.com>
2023-11-29 18:15:55 -0500

    redux persist

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,13 +3,19 @@ import App from "./list";
 import React from "react";
 import { Provider } from "react-redux";
 import store from "@/redux/store";
+import { PersistGate } from "redux-persist/integration/react";
+import { persistStore } from "redux-persist";
+
+let persistor = persistStore(store);
 
 export default function Page() {
   return (
       <Provider store={store}>
+        <PersistGate persistor={persistor}>
         <main>
           <App />
         </main>
+        </PersistGate>
       </Provider>
   );
 }
--- src/redux/store.tsx
+++ src/redux/store.tsx
@@ -1,10 +1,45 @@
 import { configureStore } from "@reduxjs/toolkit";
 import blockSlice from "@/redux/blockSlice";
+import createWebStorage from "redux-persist/lib/storage/createWebStorage";
+import { persistReducer } from "redux-persist";
+import { combineReducers } from "@reduxjs/toolkit";
+
+const createNoopStorage = () => {
+  return {
+    getItem(_key: any) {
+      return Promise.resolve(null);
+    },
+    setItem(_key: any, value: any) {
+      return Promise.resolve(value);
+    },
+    removeItem(_key: any) {
+      return Promise.resolve();
+    },
+  };
+};
+
+const storage = typeof window !== "undefined" ? createWebStorage("local") : createNoopStorage();
+
+const persistConfig = {
+  key: "root",
+  storage,
+  timeOut: null,
+}
+
+const reducer = combineReducers({
+  blocks: blockSlice,
+});
+
+const persistedReducer = persistReducer(persistConfig, reducer);
 
 const store = configureStore({
-  reducer: {
-    blocks: blockSlice,
-  },
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: ['persist/PERSIST', 'persist/REHYDRATE'],
+      },
+    }),
 });
 
 export type RootState = ReturnType<typeof store.getState>;
